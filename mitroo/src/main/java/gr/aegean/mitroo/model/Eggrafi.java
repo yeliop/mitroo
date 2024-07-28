9e74e79fed756d71259f5f32cccf61aa337386f5
yeliop <iliopoulos31@gmail.com>
2024-07-28 11:57:49 +0300

    minor changes

--- mitroo/src/main/java/gr/aegean/mitroo/model/Eggrafi.java
+++ mitroo/src/main/java/gr/aegean/mitroo/model/Eggrafi.java
@@ -1,7 +1,7 @@
 package gr.aegean.mitroo.model;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
 
 import jakarta.persistence.Basic;
